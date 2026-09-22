5c76d3dc7a5a4bc34f329c0f00054a9c4ffdecd0
samuel20072 <samueltorresospina321@>
2026-09-22 18:12:57 -0500

    ..

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'http://fumigacionangie-backend-eyzzpj-06553d-116-203-228-20.traefik.me/api/v1',
 };
